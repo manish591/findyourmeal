994715b2d543b44009d542d194b866227a3337c5
manish591 <manishdevrani777@gmail.com>
2021-09-04 22:00:18 +0530

    created js file

---
+++ app.js
@@ -0,0 +1,5 @@
+const inputField = document.querySelector('.recipename');
+const searchMealButton = document.querySelector('.search');
+const searchResultsSection = document.querySelector('.searchresultssection');
+
+let apiUrlone = 'www.themealdb.com/api/json/v1/1/categories.php';
